af67756cf28b43f5a72f1f9739e4f8e33ba31bec
David Ryman <David.ryman@hotmail.com>
2015-11-23 10:59:49 +0100

    Changed iterable to list

--- LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/framework/Reducer.java
+++ LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/framework/Reducer.java
@@ -1,5 +1,5 @@
 package org.uu.lacpp15.g3.mapreduce.framework;
 
 public interface Reducer<K2,V2,V3> {
-	public void reduce(K2 key, Iterable<V2> values, ValueEmitter<V3> emitter);
+	public void reduce(K2 key, List<V2> values, ValueEmitter<V3> emitter);
 }
