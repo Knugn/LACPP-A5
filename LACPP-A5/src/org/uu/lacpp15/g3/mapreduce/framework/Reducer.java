4d4855d7a8866608e288d378b3627b97d7694176
David Ryman <David.ryman@hotmail.com>
2015-11-23 11:00:22 +0100

    Added import of list

--- LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/framework/Reducer.java
+++ LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/framework/Reducer.java
@@ -1,5 +1,7 @@
 package org.uu.lacpp15.g3.mapreduce.framework;
 
+import java.util.List;
+
 public interface Reducer<K2,V2,V3> {
 	public void reduce(K2 key, List<V2> values, ValueEmitter<V3> emitter);
 }
