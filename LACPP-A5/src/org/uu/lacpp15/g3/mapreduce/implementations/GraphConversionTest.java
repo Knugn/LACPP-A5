43222ca5ca51f605751cda0958ec675407a0c26b
Anders Gärdenäs <Anders.gardenas@gmail.com>
2015-11-24 18:24:00 +0100

    removed additional ouptut from test

--- LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/implementations/GraphConversionTest.java
+++ LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/implementations/GraphConversionTest.java
@@ -20,7 +20,7 @@ public class GraphConversionTest {
 		//5 # 4
 		String test = "(1,3)\n(2,3)\n(2,5)\n(3,5)";
 		Map<Integer,List<String>> map = GraphConversion.run(test,10,10);
-		System.out.println(map);
+		//.out.println(map);
 		TreeMap<Integer, List<String>> tree = new TreeMap<>(map);
 		int[][] expectedValues = {{1,3},{2,3,5},{3,1,2,5},{5,2,3}};
 		int counter = 0;
@@ -36,7 +36,7 @@ public class GraphConversionTest {
 			Arrays.sort(split);
 			split[0] = temp;
 			for (int i = 0; i < split.length; i++){
-				System.out.println(split[i] + " " + expectedValues[counter][i]);
+			//	System.out.println(split[i] + " " + expectedValues[counter][i]);
 				assert(Integer.parseInt(split[i]) == expectedValues[counter][i]);
 			}
 			counter++;
