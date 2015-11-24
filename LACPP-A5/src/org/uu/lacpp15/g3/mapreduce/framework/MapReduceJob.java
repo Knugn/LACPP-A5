5d8550bd4e9874601bc9f3a464ff134dba921618
David Ryman <David.ryman@hotmail.com>
2015-11-24 10:30:56 +0100

    Added class documentation

--- LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/framework/MapReduceJob.java
+++ LACPP-A5/src/org/uu/lacpp15/g3/mapreduce/framework/MapReduceJob.java
@@ -1,6 +1,17 @@
 package org.uu.lacpp15.g3.mapreduce.framework;
 
+/**
+ * A {@link MapReduceJob} contains a description of a job that can be executed by {@link MapReduceEngine}.
+ * @author David
+ *
+ * @param <K1> Type of the input keys.
+ * @param <V1> Type of the input values.
+ * @param <K2> Type of the output keys.
+ * @param <V2> Type of intermediary values (sent from map to reduce).
+ * @param <V3> Type of the output values.
+ */
 public class MapReduceJob<K1,V1,K2,V2,V3> {
+	
 	private MapReduceIn<K1,V1> in;
 	private Mapper<K1,V1,K2,V2> mapper;
 	private Reducer<K2,V2,V3> reducer;
